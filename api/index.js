c64b1ca3ed440d9c4a7cd37c4361b4975f533ab0
SutariyaAyush <ayushsutariya44@gmail.com>
2024-05-22 19:11:34 +0530

    connect to the db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=>{
+        console.log("mongo is connected")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    
 const app=express()
+
 
 app.listen(3000,()=>{
     console.log('sever runing on 3000 port')
